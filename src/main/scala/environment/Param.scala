83cdf6bcc6a29809a57210d3fca11044043eb4d9
Kes Bhuvaneswaram <Kes.Bhuvaneswaram@office.local>
2018-07-20 16:39:54 +0100

    adding the gradle gatling framework files

---
+++ src/main/scala/environment/Param.scala
@@ -0,0 +1,27 @@
+package environment
+
+import java.io.{File, FileInputStream}
+
+import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.constructor.Constructor
+
+object Param {
+
+  val fileStream = new FileInputStream(new File(getClass.getResource("/config/params.yml").getPath))
+  val param = new Yaml(new Constructor(classOf[ParamEnvironments])).load(fileStream).asInstanceOf[ParamEnvironments]
+  fileStream.close()
+
+  def currentEnvironment: ParamEnvironment = {
+
+
+    val targetEnvironment: String = System.getProperty("targetEnvironment")
+    println ("Test Environment")
+    println (s"Target environment provided is  $targetEnvironment ")
+
+
+    if (targetEnvironment == null) {
+      throw new IllegalArgumentException("Target environment should be provided")
+    }
+    param.paramEnvironments.get(targetEnvironment)
+  }
+}
---
+++ src/main/scala/environment/ParamEnvironments.scala
@@ -0,0 +1,20 @@
+package environment
+
+import com.google.common.collect.Maps
+
+import scala.beans.BeanProperty
+import scala.collection.JavaConverters._
+
+class ParamEnvironments {
+  @BeanProperty var paramEnvironments: java.util.Map[String, ParamEnvironment] = Maps.newHashMap[String, ParamEnvironment]()
+}
+
+
+class ParamEnvironment {
+  @BeanProperty var url: String = null
+  @BeanProperty var mainPort: Int = 7990
+  @BeanProperty var bank01InPort: Int = 7991
+  @BeanProperty var bank02InPort: Int = 7992
+  @BeanProperty var bank01OutPort: Int = 7991
+  @BeanProperty var bank02OutPort: Int = 7992
+}
